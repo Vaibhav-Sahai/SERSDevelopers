3c56b7f3fc3d5c6f27a57b3d9af7badb6329418f
Vaibhav <sahaivaibhav4@gmail.com>
2019-11-26 21:13:39 +0400

    Made volunteering feature and FAQs

---
+++ SERS (1)/ViewControllerVolunteering.swift
@@ -0,0 +1,34 @@
+//
+//  ViewControllerVolunteering.swift
+//  SERS (1)
+//
+//  Created by Vaibhav Sahai on 26/11/2019.
+//  Copyright © 2019 Vaibhav Sahai. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerVolunteering: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func ExpoButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string:"https://www.expo2020dubai.com/en/programmes/volunteers")! as URL, options: [:], completionHandler: nil)
+        
+    }
+}
